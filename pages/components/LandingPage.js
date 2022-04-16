8783360744e30b1c167d28998a6dcd6da9f52ff4
Stephen Yao <stephen@koho.ca>
2022-04-16 01:00:14 -0600

    small update to ui

--- pages/components/LandingPage.js
+++ pages/components/LandingPage.js
@@ -126,25 +126,25 @@ export default function LandingPage() {
 													</button>
 												</div>
 											</form>
-											<ContractDoges />
-											<div className="flex justify-center ">
-												<div className="px-2">
-													<button
-														type="submit"
-														className="flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-													>
-														Send More Doges
-													</button>
-												</div>
+										</div>
+										<ContractDoges className="mt-6" />
+										<div className="flex justify-center ">
+											<div className="px-2">
+												<button
+													type="submit"
+													className="flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+												>
+													Send More Doges
+												</button>
+											</div>
 
-												<div className="px-2">
-													<button
-														type="submit"
-														className="flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-													>
-														Sell Dogtreats
-													</button>
-												</div>
+											<div className="px-2">
+												<button
+													type="submit"
+													className="flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+												>
+													Sell Dogtreats
+												</button>
 											</div>
 										</div>
 									</div>
